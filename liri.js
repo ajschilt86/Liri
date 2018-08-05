d8253c5a1620fd36c2dc735f1f1348cc63b60651
ajschilt <ajschilt86@gmail.com>
2018-08-05 16:26:11 -0500

    spotify blank works

--- liri.js
+++ liri.js
@@ -17,21 +17,28 @@ var client = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-
+// Switch Statements
 switch (process.argv[2]) {
+    // Twitter
     case 'my-tweets':
         var params = { screen_name: process.argv[3] };
         client.get('statuses/user_timeline', params, function (error, tweets, response) {
             if (!error) {
-
                 for (var i in tweets) {
                     console.log(tweets[i].text);
                 }
             }
         });
         break;
+    // Spotify
     case 'spotify-this-song':
-        spotify.search({ type: 'track', query: process.argv[3], limit: 1 }, function (err, data) {
+        var query = "";
+        if (process.argv[3] === undefined) {
+            query = "The Sign"
+        } else {
+            query = process.argv[3]
+        }
+        spotify.search({ type: 'track', query: query, limit: 1 }, function (err, data) {
             if (err) {
                 return console.log('Error occurred: ' + err);
             }
@@ -41,40 +48,29 @@ switch (process.argv[2]) {
             console.log("Song Url: " + data.tracks.items[0].external_urls.spotify);
         });
         break;
+    // OMDB
     case `movie-this`:
         if (process.argv[3] === undefined) {
-            var queryUrl1 = "http://www.omdbapi.com/?t=Mr-Nobody&y=&plot=short&apikey=trilogy";
+            var queryUrl = "http://www.omdbapi.com/?t=Mr-Nobody&y=&plot=short&apikey=trilogy";
 
-            request(queryUrl1, function (error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    console.log("Title: " + JSON.parse(body).Title);
-                    console.log("Released: " + JSON.parse(body).Released);
-                    console.log("Rating: " + JSON.parse(body).imdbRating);
-                    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-                    console.log("Country: " + JSON.parse(body).Country);
-                    console.log("Language: " + JSON.parse(body).Language);
-                    console.log("Plot: " + JSON.parse(body).Plot);
-                    console.log("Actors: " + JSON.parse(body).Actors);
-                }
-            });
         } else {
             var movieName = process.argv[3];
             var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-            request(queryUrl, function (error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    console.log("Title: " + JSON.parse(body).Title);
-                    console.log("Released: " + JSON.parse(body).Released);
-                    console.log("Rating: " + JSON.parse(body).imdbRating);
-                    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-                    console.log("Country: " + JSON.parse(body).Country);
-                    console.log("Language: " + JSON.parse(body).Language);
-                    console.log("Plot: " + JSON.parse(body).Plot);
-                    console.log("Actors: " + JSON.parse(body).Actors);
-                }
-            });
         }
-
+        request(queryUrl, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("Released: " + JSON.parse(body).Released);
+                console.log("Rating: " + JSON.parse(body).imdbRating);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                console.log("Country: " + JSON.parse(body).Country);
+                console.log("Language: " + JSON.parse(body).Language);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+            }
+        });
         break;
+    //Random
     case `do-what-it-says`:
         console.log(client);
         break;
